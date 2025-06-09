48f1bb8d04c19f07361d66b0f4734353a03003ea
Sanketkate385 <sanket.kate385@gmail.com>
2025-06-09 10:39:05 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,17 @@
+# Task 1: Basic Mathematical Operations
+
+
+Num1 = float(input("Enter the first number: "))
+Num2 = float(input("Enter the second number: "))
+print("Addition of Numbers:", Num1 + Num2)
+print("Subtraction of Number:", Num1 - Num2)
+print("Multiplication of Number :", Num1 * Num2)
+print("Division of Number:", Num1 / Num2)
+
+
+
+# Task 2: Personalized greeting
+
+A = input("Enter the Name:")
+B = input("Enter the sir Name:")
+print("Hello", A, B+"! Welcome to Python Programming World")
