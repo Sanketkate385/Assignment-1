cf7311668b174fcb7e504d4c0aa0bbd80dbc2d19
Sanketkate385 <sanket.kate385@gmail.com>
2025-06-09 22:16:36 +0530

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -7,11 +7,3 @@ print("Addition of Numbers:", Num1 + Num2)
 print("Subtraction of Number:", Num1 - Num2)
 print("Multiplication of Number :", Num1 * Num2)
 print("Division of Number:", Num1 / Num2)
-
-
-
-# Task 2: Personalized greeting
-
-A = input("Enter the Name:")
-B = input("Enter the sir Name:")
-print("Hello", A, B+"! Welcome to Python Programming World")
